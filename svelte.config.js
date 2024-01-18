b57328806581eff9f51538dbd69683ec916f5901
raulj <112786129+Raulj123@users.noreply.github.com>
2024-01-17 21:06:39 -0800

    chore: svelte-preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
 import dotenv from 'dotenv';
 
 dotenv.config();
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: sveltePreprocess(),
 	kit: {
 		adapter: adapter()
 	}
