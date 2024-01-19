81928ff52a6ce4e90a4a82f9504e3550e301af2b
raulj <112786129+Raulj123@users.noreply.github.com>
2024-01-18 21:34:23 -0800

    fix: forgot to add url instead of local 🤦🏽

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('Svelte app loads', async ({ page }) => {
-	await page.goto('http://localhost:5173/');
+	await page.goto('https://rjvhome.vercel.app/');
 	await page.waitForTimeout(2000);
 	const isContentPresent = await page.isVisible('.main');
 	expect(isContentPresent).toBeTruthy();
