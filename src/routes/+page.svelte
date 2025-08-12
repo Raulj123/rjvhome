080b28cfb6bfa36c34f806dce6781510b9745c17
rj <jarquinr121@gmail.com>
2025-08-11 19:31:56 -0700

    new look and umami link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import { Github, Mail, Linkedin } from 'lucide-svelte';
 </script>
 
 <div class="main">
@@ -8,10 +9,12 @@
 			<h1 class="hero-title">おはよう!</h1>
 			<p class="hero-text">
 				Hi, I'm Raul. 20 something y/o who codes professionally & for fun. I enjoy anything to do
-				with computers. Currently working on building backend services and web applications @Beckman
+				with computers. Currently building internal tools, backend services, and automation
+				solutions @Beckman
 			</p>
 			<p class="hero-text">
-				I have two dogs and play Fornite when I can. Reach out for a chat or a 1v1.
+				I have <a href="https://github.com/Raulj123/MyDogsWebsite">two dogs</a> and play Fornite when
+				I can. Reach out for a chat or a 1v1.
 			</p>
 		</div>
 		<div class="hero-section">
@@ -26,21 +29,28 @@
 	<ul style="list-style-type: none; padding:0; margin:0;">
 		<li>
 			<div class="icons">
-				<img src="/icons/github.svg" alt="github" />
+				<Github />
 				<p class="iconName"><a href="https://github.com/Raulj123/">GitHub</a></p>
 			</div>
 		</li>
 		<li>
 			<div class="icons">
-				<img src="/icons/linkedin.svg" alt="linked" />
+				<Linkedin />
 				<p class="iconName"><a href="https://www.linkedin.com/in/rauljarquin/">LinkedIn</a></p>
 			</div>
 		</li>
 		<li>
 			<div class="icons">
-				<img src="/icons/mail.svg" alt="mail" />
+				<Mail />
 				<p class="iconName"><a href="mailto:jarquinr121@gmail.com" target="_blank">Email</a></p>
 			</div>
 		</li>
 	</ul>
+</div>
+
+<div class="stalk">
+	<p>
+		Psss Im tracking you ....
+		<a href="https://umami.libme.duckdns.org/share/Dt6f3Ebkj1BHBVkS/rjvhome.vercel.app">Umami</a>
+	</p>
 </div>
